a97abf454dc058b6872552b5a70c3a7e59717849
fnyongesa943-glitch <fnyongesa943@gmail.com>
2026-03-09 11:29:42 +0300

    update user model.py
     i updated  a model in the user model.py

--- EventHive/user/models.py
+++ EventHive/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    full_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.full_name
