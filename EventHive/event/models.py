2677b582efc8838c982bbb0476d2e65bedd766a7
fnyongesa943-glitch <fnyongesa943@gmail.com>
2026-03-09 11:26:21 +0300

    update event models.py
    i updated a model in the event app model.py

--- EventHive/event/models.py
+++ EventHive/event/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db import models
+from organizer.models import Organizer
+from venue.models import Venue
 
-# Create your models here.
+class Event(models.Model):
+    event_name = models.CharField(max_length=200)
+    organizer = models.ForeignKey(Organizer, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    event_date = models.DateTimeField()
+    ticket_price = models.DecimalField(max_digits=8, decimal_places=2)
+    available_tickets = models.IntegerField()
+
+    def __str__(self):
+        return self.event_name
+
