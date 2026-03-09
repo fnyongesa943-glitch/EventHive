5f076f760524ad2bf38f68ee42ef92ae136079f2
fnyongesa943-glitch <fnyongesa943@gmail.com>
2026-03-09 13:12:41 +0300

    apps creation
    i updated the created apps in the  settings.py

---
+++ EventHive/booking/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from user.models import User
+from event.models import Event
+
+class UserBooking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    tickets_booked = models.IntegerField()
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.full_name} - {self.event.event_name}"
