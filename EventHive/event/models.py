9bcd6049049e2262f4e2f769f7d2273b764006a1
fnyongesa943-glitch <fnyongesa943@gmail.com>
2026-03-10 14:27:02 +0300

    changes in models.py,migrations
    i  edited some of the model.py then i connected the postgres database platform .i then made migrations and migrated ,created a superuser .

--- EventHive/event/models.py
+++ EventHive/event/models.py
@@ -7,7 +7,7 @@ class Event(models.Model):
     event_name = models.CharField(max_length=200)
     orginizer = models.ForeignKey(Orginizer, on_delete=models.CASCADE)
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    event_date = models.DateTimeField()
+    event_date = models.DateField()
     ticket_price = models.DecimalField(max_digits=8, decimal_places=2)
     available_tickets = models.IntegerField()
 
